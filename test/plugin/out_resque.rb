2fc326d39c7f12b2deac98516cb08afe7f7bfd15
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-04-26 16:08:50 +0900

    Fix test cases for v0.14 Compat layer

--- test/plugin/out_resque.rb
+++ test/plugin/out_resque.rb
@@ -28,18 +28,18 @@ class ResqueOutputTest < Test::Unit::TestCase
 
   def test_write
     d = create_driver
-    time = Time.at(Time.now.to_i).utc
+    time = Time.at(Time.now.to_i).utc.to_i
     d.emit({'a' => 1, "class" => "WorkerTest"}, time)
     d.emit({'b' => 2, "class" => "WorkerTest"}, time)
-    check_enqueue("test_queue", "WorkerTest", {"a" => 1, "time" => time.strftime("%y-%m-%d %H:%M:%S")})
-    check_enqueue("test_queue", "WorkerTest", {"b" => 2, "time" => time.strftime("%y-%m-%d %H:%M:%S")})
+    check_enqueue("test_queue", "WorkerTest", {"a" => 1, "time" => Time.at(time).utc.strftime("%y-%m-%d %H:%M:%S")})
+    check_enqueue("test_queue", "WorkerTest", {"b" => 2, "time" => Time.at(time).utc.strftime("%y-%m-%d %H:%M:%S")})
     d.run
     assert_equal true, true
   end
 
   def test_write_except_time_key
     d = create_driver(CONFIG + "\ninclude_time_key false")
-    time = Time.at(Time.now.to_i).utc
+    time = Time.at(Time.now.to_i).utc.to_i
     d.emit({'a' => 1, 'class' => 'WorkerTest'}, time)
     check_enqueue("test_queue", "WorkerTest", {"a" => 1})
     d.run
@@ -47,17 +47,17 @@ class ResqueOutputTest < Test::Unit::TestCase
 
   def test_write_include_tag_key
     d = create_driver(CONFIG + "\ninclude_tag_key true")
-    time = Time.at(Time.now.to_i).utc
+    time = Time.at(Time.now.to_i).utc.to_i
     d.emit({'a' => 1, 'class' => 'WorkerTest'}, time)
-    check_enqueue("test_queue", "WorkerTest", {"a" => 1, "tag" => 'test', "time" => time.strftime("%y-%m-%d %H:%M:%S")})
+    check_enqueue("test_queue", "WorkerTest", {"a" => 1, "tag" => 'test', "time" => Time.at(time).utc.strftime("%y-%m-%d %H:%M:%S")})
     d.run
   end
 
   def test_write_change_worker_class_name_tag
     d = create_driver(CONFIG + "\nworker_class_name_tag klass")
-    time = Time.at(Time.now.to_i).utc
+    time = Time.at(Time.now.to_i).utc.to_i
     d.emit({'a' => 1, 'klass' => 'WorkerTest::Test'}, time)
-    check_enqueue("test_queue", "WorkerTest::Test", {"a" => 1, "time" => time.strftime("%y-%m-%d %H:%M:%S")})
+    check_enqueue("test_queue", "WorkerTest::Test", {"a" => 1, "time" => Time.at(time).utc.strftime("%y-%m-%d %H:%M:%S")})
     d.run
   end
 end
